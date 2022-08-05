1a4d0483ebfbe262888e4296a623a7da13cd7d3f
VilattaRomina <vilattaromina@gmail.com>
2022-08-05 14:33:06 -0300

    modifique el formulario select

--- frontend/src/components/searchBlock/SearchBlock.js
+++ frontend/src/components/searchBlock/SearchBlock.js
@@ -1,28 +1,52 @@
 import React from 'react'
-import { SearchContainer, Form, SearchTitle, Button } from './SearchStyled'
+import { SearchContainer, SearchTitle, Button, Form } from './SearchStyled'
+import Select from 'react-select'
+//import { useState } from 'react'
+
+
+
+//const city = ['Salta', 'Buenos Aires', 'Córdoba', 'Santa Fe']
+
+const cities = [
+    {id:1, city:'Cordoba', country:'Argentina'}, 
+    {id:2, city:'Salta', country:'Argentina'}, 
+    {id:3, city:'Mendoza', country:'Argentina'}, 
+    {id:4, city:'Buenos Aires', country:'Argentina'}, 
+]
 
 function SearchBlock() {
 
+    // const [selectedCity, setSelectedCity] = useState()
+
+    const handleSelectChange = ({ value }) => {
+        console.log(value);
+        // setSelectedCity(value);
+    }
     return (
         <SearchContainer>
             <SearchTitle>Busca ofertas en hoteles, casas y mucho mas</SearchTitle>
-
             <Form>
-                <select required name='cities' defaultValue={{value:0}}>
-                    <option value=''>seleccionar ciudad</option>
-                    <option value="Sa">Salta</option>
-                    <option value="Bs">Buenos Aires</option>
-                    <option value="Cba">Córdoba</option>
-                </select>
+                {/* <p>Ciudad elegida: {selectedCity} </p> */}
+                <Select
+                    defaultValue={{ label: 'Selecciona una opción', value: 'empty' }}
+                
+                   options = {cities.map(item => ({label:item.city, value:item.id }
+                   ))}
+                    onChange={handleSelectChange}
+                />
+
                 <Button type='submit'>Buscar</Button>
+
             </Form>
 
 
 
-            
+
         </SearchContainer>
 
     )
 }
 
 export default SearchBlock;
+
+
