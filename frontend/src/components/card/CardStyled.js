1e3898f9d6f5f84d27580c3bfb86c2c0b5e50808
VilattaRomina <vilattaromina@gmail.com>
2022-08-05 20:54:15 -0300

    modifique estilos de la card

--- frontend/src/components/card/CardStyled.js
+++ frontend/src/components/card/CardStyled.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const CardStyle = styled.div`
 height: auto;
 width: auto;
-display: flex;
-flex-direction: row;
 background-color: white;
 border: 0.5px solid #ddd;
 padding: 20px;
@@ -12,12 +10,4 @@ border-radius: 5px;
 gap: 10px;
 margin: 10px;
 
-img{
-    width:30vh;
-    height:50vh;
-}
-
-div{
-    width:auto;
-}
 `
