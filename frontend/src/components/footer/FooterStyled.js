07fbe3a5ce19d674a50c836502ba6c918740be13
VilattaRomina <vilattaromina@gmail.com>
2022-08-05 14:33:26 -0300

    cambios en los estilos del footer

--- frontend/src/components/footer/FooterStyled.js
+++ frontend/src/components/footer/FooterStyled.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const ContainerFooter = styled.div`
 background: #1DBEB4;
 width: 100vw;
-height: 100px;
 display: flex;
 align-items: center;
 justify-content: space-between;
 position: fixed
 align-self: flex-end;
+padding:20px;
 `
 export const FooterBlock = styled.div`
 padding: 5px;
@@ -19,6 +19,6 @@ padding: 5px;
 `
 
 export const FooterIcon = styled.div`
-padding: 10px;
+padding-right:30px;
 `
 
