bf0d668deeaa937f5e57b57a0bfb7554c4d5c849
VilattaRomina <vilattaromina@gmail.com>
2022-08-05 09:59:40 -0300

    cambio de color de fondo del buscador

--- frontend/src/components/searchBlock/SearchStyled.js
+++ frontend/src/components/searchBlock/SearchStyled.js
@@ -2,17 +2,20 @@ import styled from 'styled-components';
 
 export const SearchContainer = styled.div `
 margin:0;
-background-color: #4cd4d1;
+background-color: #545776;
 `
 export const SearchTitle = styled.h1`
 margin:0;
 font size: 24px;
+text-align: center;
+padding:10px;
 `
 export const Form = styled.form `
 display: flex;
 justify-content: center;
 gap: 20px;
 margin-top: 20px;
+padding: 20px;
 
 `
 export const Button = styled.button `
