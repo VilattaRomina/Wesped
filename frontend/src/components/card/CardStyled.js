48f4663335a71a2a331df00442bf5df1d84a498e
VilattaRomina <vilattaromina@gmail.com>
2022-08-05 09:55:44 -0300

    modifique estilos en car y list

---
+++ frontend/src/components/card/CardStyled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const CardStyle = styled.div`
+height: auto;
+width: auto;
+display: flex;
+flex-direction: row;
+background-color: white;
+border: 0.5px solid #ddd;
+padding: 20px;
+border-radius: 5px;
+gap: 10px;
+margin: 10px;
+
+img{
+    width:30vh;
+    height:50vh;
+}
+
+div{
+    width:auto;
+}
+`
