5672f30603ad7cb10c9b9716b8f94282c0ea16ed
VilattaRomina <vilattaromina@gmail.com>
2022-08-06 17:40:08 -0300

    modifique estilos de la card

--- frontend/src/components/card/CardStyled.js
+++ frontend/src/components/card/CardStyled.js
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 
 export const CardStyle = styled.div`
-height: auto;
-width: auto;
+display: flex;
+font-family: "Roboto", sans-serif;
 background-color: white;
-border: 0.5px solid #ddd;
 padding: 20px;
-border-radius: 5px;
 gap: 10px;
 margin: 10px;
+border: 1px solid #f3f1ed;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+
+button{
+background-color:#1DBEB4;
+  border-radius: 5px;
+  color: white;
+  height: 40px;
+
+}
+
+img{
+    width:200px;
+    height:100%
+    border-radius: 8px;
+}
+
+
+
+
 
 `
