b2af5f57a222ccc7c8ff4415741dfaf8e9fa48de
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 12:41:59 -0300

    Cambie categories por Category

---
+++ frontend/src/components/categories/Category.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import categories from '../data/categories.json'
+import CategoryItem from './categoryItem/CategoryItem'
+import { CategoryContainerStyle, TitleStyle, CategoryListContainerStyle} from './CategoryStyles'
+
+
+export default function Categories() {
+    return (
+        <CategoryContainerStyle>
+            <TitleStyle>Buscar por tipo de alojamiento</TitleStyle>
+            <CategoryListContainerStyle>
+                {
+                    categories.map((item) =>
+                        <CategoryItem {...item} />
+                    )
+                }
+            </CategoryListContainerStyle>
+        </CategoryContainerStyle>
+    )
+}
