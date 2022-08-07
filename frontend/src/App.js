cd071d4bf7c0439bb68f5e1563dcef3ad8346f1d
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 16:31:08 -0300

    Elimine footer de App en rama categorias

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
-
-import Footer from './components/footer/Footer';
 import Categories from './components/categories/Category';
 
 function App() {
   return (
     <div>
           <Categories />
-          <Footer />
+         
     </div>
   );
 }
