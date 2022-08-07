8d798a82604c49159aa26f9f2eb556461a12430e
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 12:46:29 -0300

    Cree componente Card y agregue estlos

--- frontend/src/components/card/Card.js
+++ frontend/src/components/card/Card.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import { CardStyle } from './CardStyled'
+import { CardStyles } from './CardStyles'
 
-export default function Card({id, img, category, title, location, description}) {
-    return (
-        <CardStyle>
-            <div>
-                <img src={img} alt="img"/>
-            </div>
-            <div>
-                <h2>{category}</h2>
-                <h3>{title}</h3>
-                <p>{location}</p>
-                <p>{description}</p>
-                <button>Ver detalle</button>
-
-            </div>
-
-
-        </CardStyle>
-    )
+export default function Card({children}) {
+  return (
+    <CardStyles>
+       {children}
+    </CardStyles>
+  )
 }
