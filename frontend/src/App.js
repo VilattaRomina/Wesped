33c6a370c55d6ed3912235fd428992b3e5bcf67a
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 16:29:46 -0300

    Elimine footer de App rama Listado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import Recommendations from './components/recommendations/Recommendations';
-import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div>
           <Recommendations />
-          <Footer />
+          
     </div>
   );
 }
