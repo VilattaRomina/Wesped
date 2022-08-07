515971e6bcde813150ba93ed9cfd85bbe83f04fb
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 13:44:43 -0300

    Agregue el componente footer

---
+++ frontend/src/components/footer/FooterStyled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ContainerFooter = styled.div`
+background: #1DBEB4;
+width: 100vw;
+display: flex;
+align-items: center;
+justify-content: space-between;
+position: fixed
+align-self: flex-end;
+padding:20px;
+`
+export const FooterBlock = styled.div`
+padding: 5px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+`
+
+export const FooterIcon = styled.div`
+padding-right:30px;
+`
+
