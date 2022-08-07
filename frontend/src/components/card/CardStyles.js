5dc110c2d76cb1824d4ec73e08d8c533c0377fbf
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 13:26:59 -0300

    agregue card y elimine listado

---
+++ frontend/src/components/card/CardStyles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const CardStyles = styled.article`
+background: #ffffff;
+border: 1px solid #DFE4EA;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 0px 0px 8px 8px;
+flex:1;
+
+`
