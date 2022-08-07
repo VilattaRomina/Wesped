7fcc47cc708c594c0343982037462dbfeaff67a4
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 12:43:07 -0300

    cambie cardStyled por CategoryItemSyles

---
+++ frontend/src/components/categories/categoryItem/CategoryItemStyles.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const ItemCategoriesStyles = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const ImageCategoryStyles = styled.img`
+min-width: 100%;
+max-width: 100%;
+`;
+
+export const InfoCategoryStyle = styled.div`
+padding: 10px;
+display: flex;
+flex-direction: column;
+width:100%;
+
+`;
+
+ export const TitleCategoryStyle = styled.h3`
+ font-weight: 700;
+ font-size: 20px;
+ line-height: 23px;
+ color: #383B58;
+ padding: 0 12px 25px 12px;
+ margin:0;
+ `;
+
+export const DescriptionCategoryStyle = styled.p`
+font-weight: 700;
+font-size: 14px;
+line-height: 16px;
+color: #383B58;
+opacity: 0.6;
+margin:0;
+padding: 0 12px 6px 12px;
+`;
