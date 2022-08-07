c6765242444e253d58d8ae5cd84e09715318b2cd
VilattaRomina <vilattaromina@gmail.com>
2022-08-07 12:40:51 -0300

    Cambie CategoriesStyles por Categorystyles

---
+++ frontend/src/components/categories/CategoryStyles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const CategoryContainerStyle = styled.div`
+  background: #F3F1ED;
+  padding: 1rem 2rem 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  
+`
+export const TitleStyle = styled.h2`
+font-style: normal;
+font-weight: 700;
+font-size: 24px;
+line-height: 28px;
+color: #383B58;
+margin-bottom: 10px;
+`
+
+export const CategoryListContainerStyle = styled.div`
+    display: flex;
+    gap: 20px;
+    flex-flow: row nowrap;
+    
+    
+   
+`
+
+
+
+
+
+
+
+
+  
+  
+
+ 
+
+ 
+
+
