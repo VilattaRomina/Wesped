246e66c71c4dd28b717557b77d4b3031c2ccfdc4
VilattaRomina <vilattaromina@gmail.com>
2022-08-08 10:41:04 -0300

    elimine el borde a la card

--- frontend/src/components/card/CardStyles.js
+++ frontend/src/components/card/CardStyles.js
@@ -2,9 +2,7 @@ import styled from 'styled-components'
 
 export const CardStyles = styled.article`
 background: #ffffff;
-border: 1px solid #DFE4EA;
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 0px 0px 8px 8px;
+border-radius: 10px;
 flex:1;
-
 `
