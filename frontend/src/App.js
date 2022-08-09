26b540ed19ef996554d672933a43d78f3ee1101c
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-09 00:58:28 -0300

    Ajustes en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header";
-import Footer from './components/footer/Footer'
-import './App.css';
+import Login from "./pages/Login";
 
 function App() {
 
   return (
     <>
       <Header />
-      <Footer />
+      <Login />
     </>
   );
 
