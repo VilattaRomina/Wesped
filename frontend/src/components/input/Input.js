b8858f2275803798e2662d107d8fc1c2e76321fe
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-09 01:01:13 -0300

    Componente UI generico Input

---
+++ frontend/src/components/input/Input.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = (props) => {
+  return <StyledInput type={props.type} id={props.id} width={props.width} />;
+};
+
+export default Input;
+
+const StyledInput = styled.input`
+  width: ${(props) => props.width};
+  height: 2.5rem;
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.15);
+  border-radius: 5px;
+  border: none;
+`;
