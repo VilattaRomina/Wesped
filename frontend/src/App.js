c7434a7abc8bacdccd1b82ff45b47f74528fc373
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-10 01:31:47 -0300

    Corrijo ruteo en index.js y app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/header/Header";
+// import Header from "./components/header/Header";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -7,13 +7,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
+    <>
+      {/* <Header /> */}
+      <Routes Routes >
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+      </Routes >
+    </>
   );
 
 }
