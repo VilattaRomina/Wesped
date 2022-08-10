9189d0e9da1c1669b995a224473ad8c3e7647c0d
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-10 01:35:45 -0300

    Arreglo ruteo en index.js y app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,18 @@ import React from "react";
 import Header from "./components/header/Header";
 // import Login from "./pages/Login";
 // import Register from "./pages/Register";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter> */}
       <Header />
       {/* <Routes> */}
       {/* <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} /> */}
       {/* </Routes> */}
       <Footer />
-      {/* </BrowserRouter> */}
     </>
   );
 
