effe9431210012f75aa15e05bc9cbda39b01f7d2
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-10 16:31:59 -0300

    issue 9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import Header from "./components/header/Header";
-import Login from "./pages/Login";
+import Login from "./pages/login/Login";
 import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -10,6 +10,7 @@ function App() {
     <>
       {/* <Header /> */}
       <Routes Routes >
+        <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
       </Routes >
--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Form from '../components/form/Form'
-import LoginInput from "../components/login/LoginInput";
+import Form from '../../components/form/Form'
+import LoginInput from "./LoginInput";
 
 const Login = () => {
   return (
