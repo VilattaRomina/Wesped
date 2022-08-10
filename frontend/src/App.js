fa55546153b8868e7f8ba8b61c69ed03e884eafe
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-09 23:24:03 -0300

    Comento Login y Register pages hasta que se mergee a Develop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/header/Header";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/registro" element={<Register />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} /> */}
       </Routes>
     </BrowserRouter>
   );
