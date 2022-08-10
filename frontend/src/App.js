e36673c076dac4c11bb361e9efa1e539de13d686
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-09 23:26:49 -0300

    Comento lo referido a react-router-dom en issue #1 hasta mergear con develop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import Header from "./components/header/Header";
 // import Login from "./pages/Login";
 // import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
+      {/* <BrowserRouter> */}
       <Header />
-      <Routes>
-        {/* <Route path="/login" element={<Login />} />
+      {/* <Routes> */}
+      {/* <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} /> */}
-      </Routes>
-    </BrowserRouter>
+      {/* </Routes> */}
+      <Footer />
+      {/* </BrowserRouter> */}
+    </>
   );
 
 }
