26bf83a75b45c1a7a519259c0500c4a88e1120c7
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-09 22:49:00 -0300

    Ruteo en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Header from "./components/header/Header";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
-      <Register />
-    </>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 
 }
