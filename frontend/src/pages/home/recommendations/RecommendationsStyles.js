86f8181f9a1fc569bb0b31b4df4e757ac0404d94
VilattaRomina <vilattaromina@gmail.com>
2022-08-14 12:18:19 -0300

    cambio div por section

--- frontend/src/pages/home/recommendations/RecommendationsStyles.js
+++ frontend/src/pages/home/recommendations/RecommendationsStyles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const RecommendationContainerStyle = styled.Section`
+export const RecommendationContainerStyle = styled.section`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
