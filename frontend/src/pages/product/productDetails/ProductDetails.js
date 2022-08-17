b46231c3d26962b249074df0b2633e9154cf2a61
VilattaRomina <vilattaromina@gmail.com>
2022-08-17 10:55:54 -0300

    agrego header y huvicacion en pages product

--- frontend/src/pages/product/productDetails/ProductDetails.js
+++ frontend/src/pages/product/productDetails/ProductDetails.js
@@ -1,11 +1,31 @@
 import Carrousel from '../../../components/galleryBlock/Carrousel'
 import GalleryBlock from '../../../components/galleryBlock/GalleryBlock'
+import { HeaderStyle, Title, Arrow, UbicationStyle, BodyStyle, BlockIconStyle } from './ProductDetailsStyles'
+import { FaMapMarkerAlt, FaRegHeart, FaChevronLeft} from "react-icons/fa";
+import { BiShareAlt } from "react-icons/bi";
 
 
 export default function ProductDetails() {
   return (
-    <>
-    <GalleryBlock/>
-    </>
+    <BodyStyle>
+      <HeaderStyle>
+        <Title>
+          <h5>Categoria</h5>
+          <h4>Nombre del hotel</h4>
+        </Title>
+        <Arrow>
+          <FaChevronLeft/>
+        </Arrow>
+      </HeaderStyle>
+      <UbicationStyle>
+        <FaMapMarkerAlt />
+        <p>Ubicacion</p>
+      </UbicationStyle>
+      <BlockIconStyle>
+        <BiShareAlt />
+        <FaRegHeart />
+      </BlockIconStyle>
+      <GalleryBlock />
+    </BodyStyle>
   )
 }
