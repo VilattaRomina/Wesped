2ba623251ef797ff1190e444f33da9768d3fb268
VilattaRomina <vilattaromina@gmail.com>
2022-08-16 21:22:55 -0300

    agrego componente product details

---
+++ frontend/src/pages/product/productDetails/ProductDetails.js
@@ -0,0 +1,11 @@
+import Carrousel from '../../../components/galleryBlock/Carrousel'
+import GalleryBlock from '../../../components/galleryBlock/GalleryBlock'
+
+
+export default function ProductDetails() {
+  return (
+    <>
+    <GalleryBlock/>
+    </>
+  )
+}
