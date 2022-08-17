ea6970c601ac2bca82e41062c1a5b91ff2790b42
rainvare <indirarequiz@gmail.com>
2022-08-17 20:43:43 -0300

    creo clase image y relaciono con producto, creo imageRepository también

---
+++ BackEnd/src/main/java/com/dh/DigitalBooking/Model/Image.java
@@ -0,0 +1,44 @@
+package com.dh.DigitalBooking.Model;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "image")
+@Getter
+@Setter
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String description;
+    @Column
+    private String urlImage;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Image() {
+    }
+
+    public Image(Long id, String title, String description, String urlImage) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.urlImage = urlImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", urlImage='" + urlImage + '\'' +
+                '}';
+    }
+}
