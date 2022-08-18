54dc532a527e6ada58085ec97ad179b1c606a135
VilattaRomina <vilattaromina@gmail.com>
2022-08-18 14:36:15 -0300

    modifico estilos de pagina producto

--- frontend/src/pages/product/productDetails/ProductDetails.js
+++ frontend/src/pages/product/productDetails/ProductDetails.js
@@ -1,31 +1,43 @@
-import Carrousel from '../../../components/galleryBlock/Carrousel'
-import GalleryBlock from '../../../components/galleryBlock/GalleryBlock'
-import { HeaderStyle, Title, Arrow, UbicationStyle, BodyStyle, BlockIconStyle } from './ProductDetailsStyles'
-import { FaMapMarkerAlt, FaRegHeart, FaChevronLeft} from "react-icons/fa";
+import GalleryBlock from './galleryBlock/GalleryBlock'
+import GalleryMobile from './galleryMobile/GalleryMobile';
+import { HeaderStyle, Title, Arrow, LinkStyle, UbicationStyle, BodyStyle, BlockIconStyle, ContentIcon } from './ProductDetailsStyles'
+import { FaMapMarkerAlt, FaRegHeart, FaChevronLeft } from "react-icons/fa";
 import { BiShareAlt } from "react-icons/bi";
+//import {Link} from 'react-router-dom'
 
 
 export default function ProductDetails() {
   return (
     <BodyStyle>
+      {/* header */}
       <HeaderStyle>
-        <Title>
-          <h5>Categoria</h5>
-          <h4>Nombre del hotel</h4>
-        </Title>
-        <Arrow>
-          <FaChevronLeft/>
-        </Arrow>
+        <div>
+          <Title>
+            <h5>Categoria</h5>
+            <h4>Nombre del hotel</h4>
+          </Title>
+          <Arrow>
+            <LinkStyle to="/"> <FaChevronLeft /> </LinkStyle>
+          </Arrow>
+        </div>
       </HeaderStyle>
+ {/* bloque de ubicación */}
       <UbicationStyle>
+        <div>
         <FaMapMarkerAlt />
-        <p>Ubicacion</p>
+        <p>Ubicación</p>
+        </div>
       </UbicationStyle>
+{/* bloque de icono */}
       <BlockIconStyle>
-        <BiShareAlt />
-        <FaRegHeart />
+        <ContentIcon>
+        <div><BiShareAlt /></div>
+        <div><FaRegHeart /></div>
+        </ContentIcon>
+        
       </BlockIconStyle>
       <GalleryBlock />
+      <GalleryMobile />
     </BodyStyle>
   )
 }
--- frontend/src/pages/product/productDetails/ProductDetailsStyles.js
+++ frontend/src/pages/product/productDetails/ProductDetailsStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const BodyStyle = styled.body`
 padding-top:81px;
@@ -8,18 +9,23 @@ export const HeaderStyle = styled.div`
   height:70px;
   background-color:#0073A3;
   color: #fff;
-  padding: 15px 30px 10px 30px;
+  padding: 15px 35px 10px 30px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  display:flex;
-  justify-content: space-between;
-  align-items: center;
+ 
+  div{
+    display: flex;
+    justify-content:space-between;
+    max-width: 1300px;
+    
+    
+  }
  
 `
 
-  export const Title= styled.div`
+export const Title = styled.div`
   display:flex;
   flex-direction:column;
-
+  
     h5{
         font-size: 14px;  
         font-weight:300;
@@ -32,9 +38,15 @@ export const HeaderStyle = styled.div`
   
 `;
 
-export const Arrow= styled.div`
-font-size: 30px;
+export const Arrow = styled.div`
+font-size: 25px;
+align-items: center;
 `
+
+export const LinkStyle = styled(Link)`
+color:#fff;
+cursor:pointer;
+`;
 
 
 
@@ -42,7 +54,13 @@ export const UbicationStyle = styled.div`
 height:55px;
 background: #FDF6EC;
 padding: 20px 0 10px 30px;
-display:flex;
+
+div{
+  display:flex;
+  max-width: 1300px;
+margin: 0 auto;
+}
+
 p{
     color: black;
     font-size: 14px; 
@@ -54,7 +72,19 @@ p{
 
 export const BlockIconStyle = styled.div`
 height:50px;
-padding: 20px 0 10px 30px;
-display:flex;
+padding: 20px 0 20px 30px;
+font-size:19px;
 
 `
+export const ContentIcon = styled.div`
+display:flex;
+max-width: 1300px;
+margin: 0 auto;
+
+div{
+  margin-right:10px;
+}
+`
+
+
+
