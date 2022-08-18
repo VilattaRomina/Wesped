5637f1a590629431a30ada64e336e676b5f288d4
VilattaRomina <vilattaromina@gmail.com>
2022-08-18 14:35:05 -0300

    Agrego galeria mobile

---
+++ frontend/src/pages/product/productDetails/galleryBlock/GalleryBlock.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { ContainerStyles, ContainerImageLeft, ContainerImageRight, ImageLeft, ImageRight, CardRight, CardLeft, TextStyles, CloseModalStyle, ModalStyle} from './GalleryBlockStyles'
+import images from '../../../../data/images.json'
+import { useState } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+//import Modal from 'react-modal';
+
+
+
+export default function GalleryBlock() {
+
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+      }
+    
+
+    return (
+        <>
+            <ContainerStyles >
+                <ContainerImageLeft>
+                    <CardLeft>
+                        <ImageLeft src={images[0].img} alt="" />
+                    </CardLeft>
+                </ContainerImageLeft>
+                <ContainerImageRight>
+                    {images.slice(1, 5).map((item) => (
+                        <CardRight>
+                            <ImageRight src={item.img} alt="" />
+                        </CardRight>
+                    ))}
+                </ContainerImageRight>
+                <TextStyles onClick={openModal}>Ver mas</TextStyles>
+            </ContainerStyles>
+
+            <ModalStyle
+            isOpen={modalIsOpen}
+            //onRequestClose={closeModal}
+            >
+                <Carousel>
+                    {images.map(slide => (
+                        <div key={slide.id}>
+                            <img src={slide.img} alt="" />
+                        </div>
+                    ))}
+                </Carousel>
+
+                <CloseModalStyle onClick={closeModal}>X</CloseModalStyle>
+  
+            </ModalStyle>
+        </>
+
+    )
+}
+
+
