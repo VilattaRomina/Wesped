da10de209ee11d908beab9ce4d7bbf5610bae520
AleMarin <maialemarin@utp.edu.co>
2022-08-20 20:32:43 -0500

    Ajustes generales de BackEnd

--- BackEnd/src/main/java/com/dh/DigitalBooking/Exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dh.DigitalBooking.Exceptions;
-
-public class ResourceNotFoundException extends Exception{
-    public ResourceNotFoundException(String mensaje){
-        super (mensaje);
-    }
-
-}
