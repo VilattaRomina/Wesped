1b89b9a4911d063966127535bc8bde3d59cbef10
Juan Basualdo <basualdo.ij@gmail.com>
2022-08-23 19:45:11 -0300

    Correccion en AxiosHelper

--- frontend/src/helpers/AxiosHelper.js
+++ frontend/src/helpers/AxiosHelper.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const AxiosIntance = axios.create({
-    baseURL: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8080",
+export const AxiosInstance = axios.create({
+    baseURL: process.env.REACT_APP_API_ENDPOINT || "http://ec2-18-224-138-216.us-east-2.compute.amazonaws.com:8080",
     headers: {
         Accept: "application/json",
     },
