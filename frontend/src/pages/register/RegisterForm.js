c6b680158b970e5735200bead8fb81fa5544a400
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-05 00:44:03 -0300

    Validacion de formulario de registro (Crear cuenta) e implementacion de API externa LOCATION IQ para captar la locacion del usuario mediante el objeto navigator.geolocation y calcular su Ciudad y pasarla en el Signup

--- frontend/src/pages/register/RegisterForm.js
+++ frontend/src/pages/register/RegisterForm.js
@@ -1,18 +1,42 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import Input from '../../components/input/Input'
 import Button from '../../components/button/Button'
 import ErrorMessage from '../../components/form/ErrorMessage'
 import classes from './register.module.css'
-import { Link,useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Form, Div, Label } from '../../components/form/StyledForm'
 import validator from 'validator'
 import { AxiosInstance } from '../../helpers/AxiosHelper'
+import axios from 'axios'
 
 const RegisterForm = () => {
     const [error, setError] = useState({ visible: false, message: "" });
+    const [userLocationCity, setUserLocationCity] = useState(null);
+    const [userCoordinates, setUserCoordinates] = useState(null);
     const navigate = useNavigate();
 
-    AxiosInstance.get('/users').then(res => console.log(res));
+    useEffect(() => {
+        const success = res => {
+            setUserCoordinates({
+                latitude: res.coords.latitude,
+                longitude: res.coords.longitude
+            })
+        }
+        const error = err => console.warn(err)
+        const options = {
+            enableHighAccuracy: true,
+            timeout: 5000
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }, [])
+
+    useEffect(() => {
+        if (userCoordinates)
+            axios.get(`${process.env.REACT_APP_LOCATION_IQ_API_ENDPOINT}/reverse?key=${process.env.REACT_APP_LOCATION_IQ_TOKEN}&lat=${userCoordinates.latitude}&lon=${userCoordinates.longitude}&format=json`)
+                .then(res => setUserLocationCity(res.data.address.state))
+
+    }, [userCoordinates])
 
     const nameInputRef = useRef("");
     const surnameInputRef = useRef();
@@ -27,21 +51,25 @@ const RegisterForm = () => {
         const email = emailInputRef.current.value.trim();
         const password = passwordInputRef.current.value.trim();
         const confirmedPassword = confirmPasswordInputRef.current.value.trim();
-        
+
+        const nameIsValid = !validator.isEmpty(name);
+        const surnameIsValid = !validator.isEmpty(surname)
         const emailIsValid = validator.isEmail(email);
         const passwordLengthIsMoreThanSix = password.length >= 6;
         const areMatchingPasswords = validator.equals(confirmedPassword, password);
 
-        if (emailIsValid && passwordLengthIsMoreThanSix && areMatchingPasswords)
-        {
-            AxiosInstance.post('/users', )
-            console.log({
+        if (nameIsValid && surnameIsValid && emailIsValid && passwordLengthIsMoreThanSix && areMatchingPasswords && userLocationCity) {
+            const newUser = {
                 name: name,
                 surname: surname,
                 email: email,
-                password: password
-            })
+                password: password,
+                city: userLocationCity
+            }
+            axios.post("http://localhost:8080/auth/signup", newUser).then(res => console.log(res.data.message))
+            // TODO mostrar correctamente que el usuario se creo correctamente
             navigate('/')
+            return;
         }
 
         setError({ visible: true, message: "Por favor verifique que los datos estén correctos." })
@@ -54,21 +82,21 @@ const RegisterForm = () => {
                 <div className={classes.A}>
                     <div className={classes.B}>
                         <Label htmlFor="name">Nombre</Label>
-                        <Input width="13.5rem" type="text" id="name" required reference={nameInputRef} visible={error.visible}/>
+                        <Input width="13.5rem" type="text" id="name" required reference={nameInputRef} visible={error.visible} />
                     </div>
                     <div className={classes.B}>
                         <Label htmlFor="surname">Apellido</Label>
-                        <Input width="13.5rem" type="text" id="surname" required reference={surnameInputRef} visible={error.visible}/>
+                        <Input width="13.5rem" type="text" id="surname" required reference={surnameInputRef} visible={error.visible} />
                     </div>
                 </div>
             </Div>
             <Div>
                 <Label htmlFor="email">Correo electrónico</Label>
-                <Input width="28rem" type="email" id="email" required reference={emailInputRef} visible={error.visible}/>
+                <Input width="28rem" type="email" id="email" required reference={emailInputRef} visible={error.visible} />
                 <Label htmlFor="password">Contraseña</Label>
-                <Input width="28rem" type="password" id="password" required reference={passwordInputRef} visible={error.visible}/>
+                <Input width="28rem" type="password" id="password" required reference={passwordInputRef} visible={error.visible} />
                 <Label htmlFor="confirm-password">Confirmar contraseña</Label>
-                <Input width="28rem" type="password" id="confirm-password" required reference={confirmPasswordInputRef} visible={error.visible}/>
+                <Input width="28rem" type="password" id="confirm-password" required reference={confirmPasswordInputRef} visible={error.visible} />
                 <Div align="center">
                     <ErrorMessage visible={error.visible}>{error?.message ?? ""}</ErrorMessage>
                     <Button type="submit" width="12.5rem"><b>Crear Cuenta</b></Button>
