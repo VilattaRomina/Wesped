d4f25339348f619da186677f7a65cd8fa7086514
VilattaRomina <vilattaromina@gmail.com>
2022-09-07 22:14:00 -0300

    implemento post en pag reservas

--- frontend/src/pages/booking/Booking.js
+++ frontend/src/pages/booking/Booking.js
@@ -8,14 +8,25 @@ import Policies from '../../components/policies/Policies';
 import Section from '../../components/section/Section';
 import BookingDetail from './bookingDetail/BookingDetail';
 import BookingForm from './bookingForm/BookingForm'
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import jwt_decode from 'jwt-decode';
+import LocalStorageHelper from '../../helpers/LocalStorageHelper'
 
+function formatDate(date) {
+  console.log(date);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}-${month.toLocaleString('es-AR', { minimumIntegerDigits: 2 })}-${day.toLocaleString('es-AR', { minimumIntegerDigits: 2 })}`
+}
 
 export default function Booking(to) {
 
+  let navigate = useNavigate();
   const { productId } = useParams();
   const [product, setProduct] = useState(null);
-// peticion GET
+  // peticion GET
   useEffect(() => {
     AxiosInstance.get(`/products/${productId}`)
       .then((res) => {
@@ -36,7 +47,7 @@ export default function Booking(to) {
     errorDate: false,
     errorHour: false,
   })
-  
+
   const handleChange = (e) => {
     setValues({
       ...values,
@@ -56,9 +67,10 @@ export default function Booking(to) {
     e.preventDefault();
     let errorDate = false;
     let errorHour = false;
-    const token = "algo";
+    const token = LocalStorageHelper.getItem('Token');
 
-    const user = 1;
+    const user = jwt_decode(token);
+
 
 
     if (values.startDate === '' || values.endDate === '') {
@@ -67,68 +79,69 @@ export default function Booking(to) {
     if (values.checkInHour === '') {
       errorHour = true;
     }
-    
+
     if (errorHour || errorDate) {
       setValues({ ...values, errorDate: errorDate, errorHour: errorHour })
     } else {
 
+      
       console.log({
-        headers: { 
+        headers: {
           'Authorization': `Bearer ${token}`
         },
         data: {
           hour: `${values.checkInHour}:00`,
-          checkin: values.startDate.getFullYear() + "/" + (values.startDate.getMonth() + 1) + "/" + values.startDate.getDate(),
-          checkout: values.endDate.getFullYear() + "/" + (values.endDate.getMonth() + 1) + "/" + values.endDate.getDate(),
+          checkin: formatDate(values.startDate),
+          checkout: formatDate(values.endDate),
           product: {
             id: productId
           },
           user: {
-            id: 2
+            id: user.user_info.id
           }
         },
       });
-      /*
-      AxiosInstance.post(`/booking`, {
-        headers: { 
+
+      AxiosInstance.post(`/bookings`, {
+        hour: `${values.checkInHour}:00`,
+        checkin: formatDate(values.startDate),
+        checkout: formatDate(values.endDate),
+        product: {
+          id: productId
+        },
+        user: {
+          id: user.user_info.id
+        }
+      },
+      {
+        headers: {
           'Authorization': `Bearer ${token}`
         },
-        data: {
-          hour: values.checkInHour,
-          checkin: values.startDate,
-          checkout: values.endDate.at,
-          product: {
-            id: productId
-          },
-          user: {
-            id: 1
-          }
-        },
       })
-      .then((res) =>{
-         if (res.status === 200) {
-            useNavigate('')
-         } else if {
-          return Swal.fire ({
+        .then((res) => {
+          console.log(res.data);
+          navigate('reserva-exitosa');
+        }).catch((error) => {
+          Swal.fire({
             icon: 'error',
-            text:'Lamentablemente la reserva no ha podido realizarse. Por favor, intente mas tarde'
+            text: 'Lamentablemente la reserva no ha podido realizarse. Por favor, intente mas tarde'
           })
-         })
-        }*/
+          console.log(error);
+        })
     }
-    
 
-    
-  
 
-  
+
+
+
+
   };
 
 
-  
-  
 
- 
+
+
+
 
 
   return (
