2fbce26e968101b5be84f942c97ef6d153e43b45
rainvare <indirarequiz@gmail.com>
2022-09-12 11:27:58 -0300

    valores de inputs

--- frontend/src/pages/makeProduct/formMakeProduct/FormMakeProduct.js
+++ frontend/src/pages/makeProduct/formMakeProduct/FormMakeProduct.js
@@ -47,6 +47,85 @@ export default function FormMakeProduct({ product }) {
       });
     };
 
+    let url = AxiosInstance.post(`/products/save`);
+
+  //cities values
+  useEffect(() => {
+    if (citySelect.includes("Buenos Aires")) {
+      setCity("1");
+    } else if (citySelect.includes("San Carlos de Bariloche")) {
+      setCity("2");
+    } else if (citySelect.includes("Mendoza")) {
+      setCity("3");
+    } else if (citySelect.includes("Córdoba")) {
+      setCity("4");
+    }
+  }, [citySelect]);
+
+  //Categories values
+  useEffect(() => {
+    if (categorySelect.includes("Hoteles")) {
+      setCategory("1");
+    } else if (categorySelect.includes("Hostels")) {
+      setCategory("2");
+    } else if (categorySelect.includes("Departamentos")) {
+      setCategory("3");
+    } else if (categorySelect.includes("Bed&Breakfast")) {
+      setCategory("4");
+    }
+  }, [categorySelect]);
+
+  const validator = (e) => {
+    e.preventDefault();
+    let agregar = document.querySelectorAll("#atributes");
+    let atributesArray = [];
+    agregar.forEach((element) => {
+      atributesArray.push(element.value);
+    });
+
+    // Attributes
+    let newAtributesArray = [];
+    atributesArray.forEach((element) => {
+      console.log(element);
+      if (element.includes("Wifi")) {
+        newAtributesArray.push(1);
+      } else if (element.includes("Piscina")) {
+        newAtributesArray.push(2);
+      } else if (element.includes("Accesible")) {
+        newAtributesArray.push(3);
+      } else if (element.includes("Acepta Animales")) {
+        newAtributesArray.push(4);
+      } else if (element.includes("Restaurante")) {
+        newAtributesArray.push(5);
+      } else if (element.includes("Climatizado")) {
+        newAtributesArray.push(6);
+      } else if (element.includes("Estacionamiento")) {
+        newAtributesArray.push(7);
+      }
+    });
+
+    //images
+    let imagenes = document.querySelectorAll(".image-product");
+    let imagenesArray = [];
+    imagenes.forEach((element) => {
+      imagenesArray.push(element.value);
+    });
+
+    let objectf = (urlImage) => {
+      let object = {
+        titleImage: "habitación1",
+        descriptionImage:
+          "Lorem ipsum dolor sit amet. Eum quibusdam galisum eum autem",
+        urlImage: urlImage,
+      };
+      return object;
+    };
+    let arrayObject = [];
+    imagenesArray.forEach((element) => {
+      arrayObject.push(objectf(element));
+    });
+
+
 
 
 };
