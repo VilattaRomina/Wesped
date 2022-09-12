2203ece1dce4575f658df1ea0dfa5bc7376f8128
rainvare <indirarequiz@gmail.com>
2022-09-12 11:37:34 -0300

    form

--- frontend/src/pages/makeProduct/formMakeProduct/FormMakeProduct.js
+++ frontend/src/pages/makeProduct/formMakeProduct/FormMakeProduct.js
@@ -1,6 +1,26 @@
 import React, { useState, useRef, useEffect } from "react";
-import Button from "../button/Button";
-import { AxiosInstance } from "../../helpers/AxiosHelper";
+import {
+    ContainerMPStyled,
+    TitleMPStyled,
+    FormMPStyled,
+    DivMPStyled,
+    Div2MPStyled,
+    LabelMPStyled,
+    InputMPStyled,
+    Input2MPStyled,
+    RequiredMPStyled,
+    Label2MPStyled,
+    Label3MPStyled,
+    SelectMPStyled,
+    Select2MPStyled,
+    OptionMPStyled,
+    MoreMPStyled,
+    ContainerPoliciesStyled,
+    CardMPStyled,
+    ButtonMPStyled 
+  } from "./FormMakeProductStyled";
+import Button from "../../../components/button/Button";
+import { AxiosInstance } from "../../../helpers/AxiosHelper";
 import swal from "sweetalert2";
 
 export default function FormMakeProduct({ product }) {
@@ -77,9 +97,9 @@ export default function FormMakeProduct({ product }) {
 
   const validator = (e) => {
     e.preventDefault();
-    let agregar = document.querySelectorAll("#atributes");
+    let add = document.querySelectorAll("#atributes");
     let atributesArray = [];
-    agregar.forEach((element) => {
+    add.forEach((element) => {
       atributesArray.push(element.value);
     });
 
@@ -105,10 +125,10 @@ export default function FormMakeProduct({ product }) {
     });
 
     //images
-    let imagenes = document.querySelectorAll(".image-product");
-    let imagenesArray = [];
-    imagenes.forEach((element) => {
-      imagenesArray.push(element.value);
+    let images = document.querySelectorAll(".image-product");
+    let imagesArray = [];
+    images.forEach((element) => {
+      imagesArray.push(element.value);
     });
 
     let objectf = (urlImage) => {
@@ -121,8 +141,8 @@ export default function FormMakeProduct({ product }) {
       return object;
     };
     let arrayObject = [];
-    imagenesArray.forEach((element) => {
-      arrayObject.push(objectf(element));
+    imagesArray.forEach((e) => {
+      arrayObject.push(objectf(e));
     });
     //alerts and formats
     swal({
@@ -180,14 +200,249 @@ export default function FormMakeProduct({ product }) {
   };
 
   const handleClickAtributes = () => {
-    atributes.current.innerHTML += `<div class="divImage"><input id="zuliban1" value=${atributeSelect} class="selectTake" type="text"/><div class="clear">x</div></div>`;
+    atributes.current.innerHTML += `<div class="divImage">
+    <input id="div-input" value=${atributeSelect} class="selectTake" type="text"/><div class="clear">
+    x
+    </div></div>`;
     clearproduct();
   };
   const handleClickImages = () => {
     images.current.style.width = "100%";
 
-    images.current.innerHTML += `<div class="divImage"><input id="zuliban" class="zull" value=${urlImage} class="input2" type="text"/><div class="clear">x</div></div>`;
+    images.current.innerHTML += `<div class="divImage">
+    <input id="div-input" class="input-div" value=${urlImage} class="input-url" type="text"/><div class="clear">
+    x
+    </div></div>`;
     setUrlImage("https://");
     clearproduct();
   };
+
+  return (
+    <>
+      <ContainerMPStyled>
+        <TitleMPStyled>Crear Propiedad</TitleMPStyled>
+
+        <FormMPStyled ref={form} onSubmit={validator}>
+          <DivMPStyled>
+            <div>
+              <LabelMPStyled htmlFor="name">Nombre del producto</LabelMPStyled>
+              <InputMPStyled
+                required
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+              <RequiredMPStyled ref={validateName} style={{ display: "none" }}>
+                Campo obligatorio
+              </RequiredMPStyled>
+            </div>
+            <div>
+              <Label2MPStyled htmlFor="category">Categoria</Label2MPStyled>
+
+              <Select2MPStyled
+                onChange={(e) => {
+                  setCategorySelect(e.target.value);
+                }}
+              >
+                <OptionMPStyled>Hotel</OptionMPStyled>
+                <OptionMPStyled>Hostels</OptionMPStyled>
+                <OptionMPStyled>Break & Breakfast</OptionMPStyled>
+                <OptionMPStyled>Departamentos</OptionMPStyled>
+              </Select2MPStyled>
+            </div>
+          </DivMPStyled>
+
+          <DivMPStyled>
+            <div className="margin-right-form-sing-up">
+              <LabelMPStyled htmlFor="direction">Dirección</LabelMPStyled>
+              <InputMPStyled
+                required
+                type="text"
+                id="direction"
+                name="direction"
+                value={direction}
+                onChange={(e) => {
+                  setDirection(e.target.value);
+                }}
+              />
+              <RequiredMPStyled ref={validateEmail} style={{ display: "none" }}>
+                Campo obligatorio
+              </RequiredMPStyled>
+            </div>
+            <div>
+              <Label2MPStyled htmlFor="city">Ciudad</Label2MPStyled>
+
+              <Select2MPStyled
+                value={citySelect}
+                onChange={(e) => {
+                  setCitySelect(e.target.value);
+                }}
+              >
+                <OptionMPStyled>Buenos Aires</OptionMPStyled>
+                <OptionMPStyled>San Carlos de Bariloche</OptionMPStyled>
+                <OptionMPStyled>Mendoza</OptionMPStyled>
+                <OptionMPStyled>Córdoba</OptionMPStyled>
+              </Select2MPStyled>
+            </div>
+          </DivMPStyled>
+
+          <DivMPStyled>
+            <div>
+              <LabelMPStyled htmlFor="latitude">
+                Latitud (Coordenada)
+              </LabelMPStyled>
+
+              <InputMPStyled
+                type="text"
+                id="latitude"
+                name="latitude"
+                value={latitude}
+                onChange={(e) => {
+                  setLatitude(e.target.value);
+                }}
+              />
+              <RequiredMPStyled
+                ref={validateLatitude}
+                style={{ display: "none" }}
+              >
+                Campo obligatorio
+              </RequiredMPStyled>
+            </div>
+
+            <div>
+              <LabelMPStyled htmlFor="longitude">
+                Longitud (Coordenada)
+              </LabelMPStyled>
+
+              <InputMPStyled
+                type="text"
+                id="longitude"
+                name="longitude"
+                value={longitude}
+                onChange={(e) => {
+                  setLongitude(e.target.value);
+                }}
+              />
+              <RequiredMPStyled
+                ref={validateLongitude}
+                style={{ display: "none" }}
+              >
+                Campo obligatorio
+              </RequiredMPStyled>
+            </div>
+          </DivMPStyled>
+
+          <LabelMPStyled htmlFor="description">Descripción</LabelMPStyled>
+          <textarea
+            id="description"
+            name="description"
+            required
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+
+          <RequiredMPStyled
+            ref={validateDescription}
+            style={{ display: "none" }}
+          >
+            Campo obligatorio
+          </RequiredMPStyled>
+
+          <TitleMPStyled>Agregar atributos</TitleMPStyled>
+
+          <div ref={atributes}></div>
+          <Div2MPStyled>
+            <SelectMPStyled
+              value={atributeSelect}
+              onChange={(e) => {
+                setAtributeSelect(e.target.value);
+              }}
+            >
+              <OptionMPStyled>Wifi</OptionMPStyled>
+              <OptionMPStyled>Piscina</OptionMPStyled>
+              <OptionMPStyled>Accesible</OptionMPStyled>
+              <OptionMPStyled>Acepta Animales</OptionMPStyled>
+              <OptionMPStyled>Restaurante</OptionMPStyled>
+              <OptionMPStyled>Climatizado</OptionMPStyled>
+              <OptionMPStyled>Estacionamiento</OptionMPStyled>
+            </SelectMPStyled>
+
+            <MoreMPStyled onClick={handleClickAtributes}>+</MoreMPStyled>
+          </Div2MPStyled>
+
+          <TitleMPStyled>Políticas del producto</TitleMPStyled>
+
+          <ContainerPoliciesStyled>
+            <CardMPStyled>
+              <Label3MPStyled>Normas de la casa</Label3MPStyled>
+              <LabelMPStyled htmlFor="policy">Descripción</LabelMPStyled>
+              <textarea
+                id="description-policies"
+                name="description"
+                value={description1}
+                onChange={(e) => {
+                  setDescription1(e.target.value);
+                }}
+              />
+            </CardMPStyled>
+
+            <CardMPStyled>
+              <Label3MPStyled>Salud y Seguridad</Label3MPStyled>
+              <LabelMPStyled htmlFor="security">Descripción</LabelMPStyled>
+              <textarea
+                id="description-security"
+                name="description"
+                value={description2}
+                onChange={(e) => {
+                  setDescription2(e.target.value);
+                }}
+              />
+            </CardMPStyled>
+
+            <CardMPStyled>
+              <Label3MPStyled>Política de cancelación</Label3MPStyled>
+              <LabelMPStyled htmlFor="cancelation">Descripción</LabelMPStyled>
+              <textarea
+                id="description-cancelation"
+                name="description"
+                value={description3}
+                onChange={(e) => {
+                  setDescription3(e.target.value);
+                }}
+              />
+            </CardMPStyled>
+          </ContainerPoliciesStyled>
+
+          <TitleMPStyled>Cargar imágenes</TitleMPStyled>
+
+          <div ref={images}></div>
+          <Div2MPStyled>
+            <Input2MPStyled
+              id="img-product-by-admin"
+              placeholder="insertar https://"
+              type="text"
+              name="urlImage"
+              value={urlImage}
+              onChange={(e) => {
+                setUrlImage(e.target.value);
+              }}
+            />
+
+            <MoreMPStyled onClick={handleClickImages}>+</MoreMPStyled>
+          </Div2MPStyled>
+          <ButtonMPStyled>
+            <Button type="submit" width="12.5rem">
+              Crear
+            </Button>
+          </ButtonMPStyled>
+        </FormMPStyled>
+      </ContainerMPStyled>
+    </>
+  );
 }
