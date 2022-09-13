df536df59b31639791218b8077fca4c988ca3aab
VilattaRomina <vilattaromina@gmail.com>
2022-09-13 14:31:05 -0300

    creo pad de formulario

--- frontend/src/pages/makeProduct/MakeProduct.js
+++ frontend/src/pages/makeProduct/MakeProduct.js
@@ -1,15 +1,33 @@
 import React from 'react'
-import FormMakeProduct from './formMakeProduct/FormMakeProduct'
 import Section from '../../components/section/Section'
 import Body from '../../components/body/Body'
+import { BoxHeaderStyle,HeaderStyle, Title, Arrow, LinkStyle, FormBodyStyle, TitleContainer } from './MakeProductStyle'
+import { FaChevronLeft } from "react-icons/fa";
+import FormMakeProduct from './formMakeProduct/FormMakeProduct';
+
 
 export default function MakeProduct() {
   return (
-   <Section>
-    <Body>
-     <FormMakeProduct />
-    </Body>
-   </Section>
-   
+    <Section>
+      <Body>
+      <BoxHeaderStyle>
+          <HeaderStyle>
+            <Title>
+              <h2>Administración de productos</h2>
+            </Title>
+            <Arrow>
+              <LinkStyle to={""}>
+                <FaChevronLeft />
+              </LinkStyle>
+            </Arrow>
+          </HeaderStyle>
+          </BoxHeaderStyle>
+        <FormBodyStyle>
+        <TitleContainer>Crear Propiedad</TitleContainer>
+        <FormMakeProduct/>
+        </FormBodyStyle>
+      </Body>
+    </Section>
+
   )
 }
