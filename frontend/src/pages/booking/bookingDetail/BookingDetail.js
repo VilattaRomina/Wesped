4eaf99fcde7a6a17c6078ebd1be74e6f2fb84b38
Juan Basualdo <basualdo.ij@gmail.com>
2022-09-20 21:39:04 -0300

    Cambio logica en BookingDetails para que tome values que vengan desde SelectedDatesContext

--- frontend/src/pages/booking/bookingDetail/BookingDetail.js
+++ frontend/src/pages/booking/bookingDetail/BookingDetail.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { CardStyle, ContainerStyle, ContainerImage, Title, Image, InfoDetailProduct, ContainerButton, ContainerDate } from './BookingDetailStyle';
 import Button from '../../../components/button/Button'
-import {HiLocationMarker} from 'react-icons/hi'
+import { HiLocationMarker } from 'react-icons/hi'
 import { useContext } from 'react';
 import { SelectedDatesContext } from '../../../hooks/UseContext';
 import { toUserReadableDateString } from '../../../hooks/UseToString';
 
-export default function BookingDetail({ handleSubmit, product, images, startDate, endDate}) {
+export default function BookingDetail({ handleSubmit, product, images }) {
   const { selectedDatesContext } = useContext(SelectedDatesContext)
 
   return (
@@ -22,17 +22,17 @@ export default function BookingDetail({ handleSubmit, product, images, startDate
           <InfoDetailProduct >
             <h3>{product.category.title}</h3>
             <h2>{product.title}</h2>
-            <p><span><HiLocationMarker/></span>{product.city.name + ", " + product.city.country}</p>
+            <p><span><HiLocationMarker /></span>{product.city.name + ", " + product.city.country}</p>
           </InfoDetailProduct>
           <hr />
           <ContainerDate>
             <h4>Check in</h4>
-            <div>{selectedDatesContext ? toUserReadableDateString(selectedDatesContext?.checkin)  : null }</div>
+            <div>{selectedDatesContext ? toUserReadableDateString(selectedDatesContext?.checkin) : null}</div>
           </ContainerDate>
           <hr />
           <ContainerDate>
             <h4>Check out</h4>
-            <div>{selectedDatesContext ? toUserReadableDateString(selectedDatesContext?.checkout)  : null }</div>
+            <div>{selectedDatesContext ? toUserReadableDateString(selectedDatesContext?.checkout) : null}</div>
           </ContainerDate>
           <hr />
           <ContainerButton>
